035cd982e20fd9ea7601776c5d2cacf7cb5e1387
Kirill Dubovikov <dubovikov.kirill@gmail.com>
2017-03-03 01:44:54 +0300

    added variables to main playbook

--- provision.sh
+++ provision.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -euf -o pipefail
 
-ANSIBLE_HOST_KEY_CHECKING=False 
 ANSIBLE_HOSTS=`pwd`/ec2/ec2.py
 EC2_INI_PATH=`pwd`/ec2/ec2.ini
-ansible-playbook ec2.yml --user ec2-user -vvvvv
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook ec2.yml --user ec2-user -vvvvv
